cddc9aed5e2e8c9017a8f58e1878ed26b5e40a7b
mludowise <mludowise@gmail.com>
2014-10-19 23:32:54 -0700

    Catching up with exercise...

--- Facebook/LoginViewController.swift
+++ Facebook/LoginViewController.swift
@@ -8,9 +8,18 @@
 
 import UIKit
 
+private let kEmail = "mel@melludowise.com"
+private let kPassword = "password"
+
+private let kLoginSegueID = "loginSegue"
+
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginComponentsView: UIView!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     private var loginYPos : CGFloat?
     private var loginHeight : CGFloat?
@@ -29,37 +38,32 @@ class LoginViewController: UIViewController {
 
     func keyboardWillShow(notification: NSNotification!) {
         var userInfo = notification.userInfo!
-        
-        // Get the keyboard height and width from the notification
-        // Size varies depending on OS, language, orientation
         var kbSize = (userInfo[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue().size
-        
-        var durationValue = userInfo[UIKeyboardAnimationDurationUserInfoKey] as NSNumber
-        var animationDuration = durationValue.doubleValue
-        var curveValue = userInfo[UIKeyboardAnimationCurveUserInfoKey] as NSNumber
-        var animationCurve = curveValue.integerValue
-        
-        UIView.animateWithDuration(animationDuration, delay: 0.0, options: UIViewAnimationOptions.fromRaw(UInt(animationCurve << 16))!, animations: {
-            
-            self.loginComponentsView.frame.origin.y = self.screenHeight! - kbSize.height - self.loginHeight!
-            }, completion: nil)
+        self.loginComponentsView.frame.origin.y = self.screenHeight! - kbSize.height - self.loginHeight!
     }
     
     func keyboardWillHide(notification: NSNotification!) {
         var userInfo = notification.userInfo!
-        
-        // Get the keyboard height and width from the notification
-        // Size varies depending on OS, language, orientation
         var kbSize = (userInfo[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue().size
-        var durationValue = userInfo[UIKeyboardAnimationDurationUserInfoKey] as NSNumber
-        var animationDuration = durationValue.doubleValue
-        var curveValue = userInfo[UIKeyboardAnimationCurveUserInfoKey] as NSNumber
-        var animationCurve = curveValue.integerValue
-        
-        UIView.animateWithDuration(animationDuration, delay: 0.0, options: UIViewAnimationOptions.fromRaw(UInt(animationCurve << 16))!, animations: {
-            
-            self.loginComponentsView.frame.origin.y = self.loginYPos!
-            
-            }, completion: nil)
+        self.loginComponentsView.frame.origin.y = self.loginYPos!
+    }
+    
+    @IBAction func onEditingChanged(sender: UITextField) {
+        loginButton.enabled = emailField.text != "" && passwordField.text != ""
+    }
+    
+    @IBAction func onLoginButton(sender: AnyObject) {
+        println("button")
+        activityIndicator.startAnimating()
+        loginButton.selected = true
+        delay(2, { () -> () in
+            if (self.emailField.text != kEmail || self.passwordField.text != kPassword) {
+                self.loginButton.selected = false
+                var alert = UIAlertView(title: "Incorrect Email or Password", message: "", delegate: nil, cancelButtonTitle: "OK")
+                alert.show()
+            } else {
+                self.performSegueWithIdentifier(kLoginSegueID, sender: self)
+            }
+        })
     }
 }
---
+++ Facebook/Util.swift
@@ -0,0 +1,18 @@
+//
+//  Util.swift
+//  Facebook
+//
+//  Created by Mel Ludowise on 10/19/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+import Foundation
+
+func delay(delay:Double, closure:()->()) {
+    dispatch_after(
+        dispatch_time(
+            DISPATCH_TIME_NOW,
+            Int64(delay * Double(NSEC_PER_SEC))
+        ),
+        dispatch_get_main_queue(), closure)
+}
